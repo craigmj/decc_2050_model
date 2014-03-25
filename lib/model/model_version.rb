f94d0f2335afd5fb96d4f738c91de943c1797f5e
Craig Mason-Jones <craig@lateral.co.za>
2014-03-26 00:15:37 +0200

    Spreadsheet of 25 March midnight

--- lib/model/model_version.rb
+++ lib/model/model_version.rb
@@ -1,3 +1,1 @@
-def Model.last_modified_date() @last_modified_date ||= Time.utc(*[2, 43, 18, 20, 3, 2014, 4, 79, false, "SAST"]); end
-def Model.version() "0.71.0 20140323 12h27" end
-	
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[38, 14, 0, 26, 3, 2014, 3, 85, false, "SAST"]); end
