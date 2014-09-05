02a18ab353e0760b050d44376aa0bbdcc9334db4
craigmj <craig@lateral.co.za>
2014-09-05 10:20:08 +0200

    Newer spreadsheet

--- lib/model/model_version.rb
+++ lib/model/model_version.rb
@@ -1,1 +1,1 @@
-def Model.last_modified_date() @last_modified_date ||= Time.utc(*[11, 9, 21, 4, 9, 2014, 4, 247, false, "SAST"]); end
+def Model.last_modified_date() @last_modified_date ||= Time.utc(*[4, 36, 7, 5, 9, 2014, 5, 248, false, "SAST"]); end
